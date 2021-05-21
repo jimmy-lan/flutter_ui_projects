e71a326371fbc21b65eb211980f3dfce2cb91117
Jimmy <jimmylanbcn@gmail.com>
2021-05-21 19:55:22 -0400

    Add menu svg

--- exercise_app/lib/main.dart
+++ exercise_app/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:exercise_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import './constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,10 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        fontFamily: "Cairo",
-        scaffoldBackgroundColor: kBackgroundColor,
-        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor)
-      ),
+          fontFamily: "Cairo",
+          scaffoldBackgroundColor: kBackgroundColor,
+          textTheme:
+              Theme.of(context).textTheme.apply(displayColor: kTextColor)),
       home: HomeScreen(),
     );
   }
@@ -25,15 +27,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(body: Stack(children: [
-      Container(
-        height: size.height * .45,
-        decoration: BoxDecoration(color: Color(0xFFF5CeB8), image: DecorationImage(
-          alignment: Alignment.centerLeft,
-          image: AssetImage("assets/images/undraw_pilates_gpdb.png")
-        ))
-      )
-    ],),);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+              height: size.height * .45,
+              decoration: BoxDecoration(
+                  color: Color(0xFFF5CeB8),
+                  image: DecorationImage(
+                      alignment: Alignment.centerLeft,
+                      image: AssetImage(
+                          "assets/images/undraw_pilates_gpdb.png")))),
+          Column(
+            children: [
+              Container(
+                height: 52,
+                width: 52,
+                decoration: BoxDecoration(
+                    color: Color(0xFFF2BEA1),
+                    shape: BoxShape.circle
+                ),
+                child: SvgPicture.asset("assets/icons/menu.svg"),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
-
