64bae66108e4df8d2c92460c0cfb0a0f5e0816c4
Jimmy <jimmylanbcn@gmail.com>
2021-05-22 17:08:50 -0400

    Add search field

--- exercise_app/lib/main.dart
+++ exercise_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import './constants.dart';
@@ -60,6 +61,19 @@ class HomeScreen extends StatelessWidget {
                       .textTheme
                       .headline4
                       .copyWith(fontWeight: FontWeight.w900),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.6)),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      icon: SvgPicture.asset("assets/icons/search.svg"),
+                      border: InputBorder.none,
+                    ),
+                  ),
                 )
               ],
             ),
